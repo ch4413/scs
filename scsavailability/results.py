6453b7f17eb39748a5de4d18235e8a940dc68c97
ch4413 <christopher.hughes@bath.edu>
2020-11-25 18:38:07 +0000

    add frame and package

---
+++ scsavailability/results.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from datetime import datetime
+
+def create_results_table():
+    """
+    Summary
+    -------
+    Creates table output of model r2 value with metadata and timestamp
+    Parameters
+    ----------
+    model: ModelClass
+        fitted model
+    start_date: String
+        String form of date to filter by in form YYYY-MM-DD
+    Returns
+    -------
+    df_nielsen_clean: pandas DataFrame
+        The cleaned and transformed Nielsen data.
+    Example
+    --------
+    df_nielsen_clean = process_model_data(df_nielsen)
+    """
+
+    df_results = pd.DataFrame()
+
+    return df_results
+    
+
