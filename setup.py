b282bb6a6b728814b1fc8cbd2d719932a9774c41
ch4413 <christopher.hughes@bath.edu>
2020-12-09 18:07:22 +0000

    add manifest

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='scsavailability',
       version='0.1',
       description='Python SCS Availability',
       author='Chris Hughes',
       author_email='christopher.hughes@bath.edu',
+      include_package_data=True,
       url='https://www.python.org',
-      packages=['scsavailability',
-               'scsavailability.data',
-               'scsavailability.test']
+      packages=find_packages()
      )
      
